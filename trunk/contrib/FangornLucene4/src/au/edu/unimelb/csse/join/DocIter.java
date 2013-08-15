0b7ca2cf789c68c106cab201e24b623e4a1c3178
sumukh.ghodke <sumukh.ghodke@040604f4-c67c-0ffc-d1fe-39430aa826c6>
2013-08-15 18:46:55 +0000

    added term freq analyser

--- trunk/contrib/FangornLucene4/src/au/edu/unimelb/csse/join/DocIter.java
+++ trunk/contrib/FangornLucene4/src/au/edu/unimelb/csse/join/DocIter.java
@@ -1,9 +1,12 @@
 package au.edu.unimelb.csse.join;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 // for performance testing only
 public class DocIter extends AbstractJoin {
+	Map<Integer, Integer> pfIndex = new HashMap<Integer, Integer>();
 
 	public DocIter(String[] labels) {
 		super(labels, null);
@@ -12,7 +15,19 @@ public class DocIter extends AbstractJoin {
 	@Override
 	public void setupPerDoc() throws IOException {
 		// TODO Auto-generated method stub
-		
+	}
+	
+	public int getTermFreq(int idx) throws IOException {
+		return postingsFreqs[pfIndex.get(idx)].postings.freq();
+	}
+	
+	@Override
+	public void setupPerAtomicContext() {
+		super.setupPerAtomicContext();
+		pfIndex.clear();
+		for (int i = 0; i < postingsFreqs.length; i++) {
+			pfIndex.put(postingsFreqs[i].position, i);
+		}
 	}
 
 }
