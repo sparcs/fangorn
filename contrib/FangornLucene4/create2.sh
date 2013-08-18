49d7882b9cd05bbf20251d896d3b60578c2c8af4
sumukh.ghodke@gmail.com <sumukh.ghodke@gmail.com@040604f4-c67c-0ffc-d1fe-39430aa826c6>
2013-08-18 08:04:24 +0000

    added inverse queries and script to generate other data

--- contrib/FangornLucene4/create2.sh
+++ contrib/FangornLucene4/create2.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-echo `date`;
-java -cp .:./fangornL4.jar:./lib/lucene-core-4.0-SNAPSHOT.jar -Xmx6144m au.edu.unimelb.csse.CreateIndex /home/sumukh/data/wiki-char-john-T50-proc /opt/wiki-index-all > >(tee stdout.log) 2> >(tee stderr.log >&2)
-echo `date`;
+echo "creating subset indexes"
+for i in 1 2 3; do
+echo "Index ${i} start time `date`";
+java -cp .:./fangornL4.jar:./lib/lucene-core-4.2.1.jar -Xmx6144m au.edu.unimelb.csse.CreateIndex /home/sumukh/data/wiki-char-john-T50-proc-${i} /opt/wiki-index-${i} > >(tee stdout${i}.log) 2> >(tee stderr${i}.log >&2)
+echo "Index ${i} end time `date`";
+done;
 
