f7a677cee5f24bcfa3a76a8ba380dc4beaf142a6
sumukh.ghodke@gmail.com <sumukh.ghodke@gmail.com@040604f4-c67c-0ffc-d1fe-39430aa826c6>
2013-08-18 08:04:24 +0000

    added inverse queries and script to generate other data

--- trunk/contrib/FangornLucene4/src/au/edu/unimelb/csse/exp/Queries.java
+++ trunk/contrib/FangornLucene4/src/au/edu/unimelb/csse/exp/Queries.java
@@ -262,6 +262,193 @@ class Queries {
 					Operator.IMMEDIATE_FOLLOWING_SIBLING,
 					Operator.FOLLOWING_SIBLING);
 			break;
+			//Reverse queries start from here
+		case 45:
+			q.setLabels("VP", "NP");
+			q.setParents(-1, 0);
+			q.setOperators(Operator.DESCENDANT, Operator.PARENT);
+			break;
+		case 46:
+			q.setLabels("VP", "NP");
+			q.setParents(-1, 0);
+			q.setOperators(Operator.DESCENDANT, Operator.ANCESTOR);
+			break;
+		case 47:
+			q.setLabels("NP", "VP");
+			q.setParents(-1, 0);
+			q.setOperators(Operator.DESCENDANT, Operator.PARENT);
+			break;
+		case 48:
+			q.setLabels("NP", "VP");
+			q.setParents(-1, 0);
+			q.setOperators(Operator.DESCENDANT, Operator.ANCESTOR);
+			break;
+		case 49:
+			q.setLabels("PP", "S");
+			q.setParents(-1, 0);
+			q.setOperators(Operator.DESCENDANT, Operator.PARENT);
+			break;
+		case 50:
+			q.setLabels("PP", "S");
+			q.setParents(-1, 0);
+			q.setOperators(Operator.DESCENDANT, Operator.ANCESTOR);
+			break;
+		case 51:
+			q.setLabels("S", "PP");
+			q.setParents(-1, 0);
+			q.setOperators(Operator.DESCENDANT, Operator.PARENT);
+			break;
+		case 52:
+			q.setLabels("S", "PP");
+			q.setParents(-1, 0);
+			q.setOperators(Operator.DESCENDANT, Operator.ANCESTOR);
+			break;
+		case 53:
+			q.setLabels("DT", "S");
+			q.setParents(-1, 0);
+			q.setOperators(Operator.DESCENDANT, Operator.PARENT);
+			break;
+		case 54:
+			q.setLabels("DT", "S");
+			q.setParents(-1, 0);
+			q.setOperators(Operator.DESCENDANT, Operator.ANCESTOR);
+			break;
+		case 55:
+			q.setLabels("S", "DT");
+			q.setParents(-1, 0);
+			q.setOperators(Operator.DESCENDANT, Operator.PARENT);
+			break;
+		case 56:
+			q.setLabels("S", "DT");
+			q.setParents(-1, 0);
+			q.setOperators(Operator.DESCENDANT, Operator.ANCESTOR);
+			break;
+		case 57:
+			q.setLabels("ADVP", "UCP");
+			q.setParents(-1, 0);
+			q.setOperators(Operator.DESCENDANT, Operator.PARENT);
+			break;
+		case 58:
+			q.setLabels("ADVP", "UCP");
+			q.setParents(-1, 0);
+			q.setOperators(Operator.DESCENDANT, Operator.ANCESTOR);
+			break;
+		case 59:
+			q.setLabels("UCP", "ADVP");
+			q.setParents(-1, 0);
+			q.setOperators(Operator.DESCENDANT, Operator.PARENT);
+			break;
+		case 60:
+			q.setLabels("UCP", "ADVP");
+			q.setParents(-1, 0);
+			q.setOperators(Operator.DESCENDANT, Operator.ANCESTOR);
+			break;
+		case 61:
+			q.setLabels("RB", "CONJP");
+			q.setParents(-1, 0);
+			q.setOperators(Operator.DESCENDANT, Operator.PARENT);
+			break;
+		case 62:
+			q.setLabels("these", "PP");
+			q.setParents(-1, 0);
+			q.setOperators(Operator.DESCENDANT, Operator.ANCESTOR);
+			break;
+		case 63:
+			q.setLabels("at", "WHPP");
+			q.setParents(-1, 0);
+			q.setOperators(Operator.DESCENDANT, Operator.ANCESTOR);
+			break;
+		case 64:
+			q.setLabels("PRN", "ADJP", "PP", ",");
+			q.setParents(-1, 0, 0, 0);
+			q.setOperators(Operator.DESCENDANT, Operator.DESCENDANT,
+					Operator.DESCENDANT, Operator.DESCENDANT);
+			break;
+		case 65:
+			q.setLabels("ADJP", "VP", "JJ", "PP", "NP");
+			q.setParents(-1, 0, 1, 1, 3);
+			q.setOperators(Operator.DESCENDANT, Operator.PARENT, Operator.CHILD, Operator.CHILD, Operator.CHILD);
+			break;
+		case 66:
+			q.setLabels("VBN", "NP", "PP", "IN", "VP", "S");
+			q.setParents(-1, 0, 1, 2, 2, 4);
+			q.setOperators(Operator.DESCENDANT, Operator.PARENT, Operator.ANCESTOR, Operator.CHILD, Operator.DESCENDANT, Operator.CHILD);
+			break;
+		case 67:
+			q.setLabels("VBN", "NP", "PP", "IN", "VP", "S");
+			q.setParents(-1, 0, 1, 2, 2, 4);
+			q.setOperators(Operator.DESCENDANT, Operator.PARENT, Operator.PARENT, Operator.CHILD, Operator.CHILD, Operator.CHILD);
+			break;
+		case 68:
+			q.setLabels("VBN", "NP", "CD", "PP", "S", "VP", "IN", "NN");
+			q.setParents(-1, 0, 1, 1, 3, 4, 3, 3);
+			q.setOperators(Operator.DESCENDANT, Operator.PARENT, Operator.ANCESTOR, Operator.ANCESTOR, Operator.ANCESTOR, Operator.CHILD, Operator.CHILD, Operator.DESCENDANT);
+			break;
+		case 69:
+			q.setLabels("VBN", "NP", "CD", "PP", "S", "VP", "NN", "IN");
+			q.setParents(-1, 0, 1, 1, 3, 4, 3, 3);
+			q.setOperators(Operator.DESCENDANT, Operator.PARENT, Operator.ANCESTOR, Operator.ANCESTOR, Operator.ANCESTOR, Operator.CHILD, Operator.DESCENDANT, Operator.CHILD);
+			break;
+		case 70:
+			q.setLabels("PP", "NP", "VP");
+			q.setParents(-1, 0, 1);
+			q.setOperators(Operator.DESCENDANT, Operator.ANCESTOR, Operator.ANCESTOR);
+			break;
+		case 71:
+			q.setLabels("of", "PP", "NP", "VP");
+			q.setParents(-1, 0, 1, 2);
+			q.setOperators(Operator.DESCENDANT, Operator.ANCESTOR, Operator.ANCESTOR, Operator.ANCESTOR);
+			break;
+		case 72:
+			q.setLabels("of", "PP", "NP", "VP");
+			q.setParents(-1, 0, 1, 2);
+			q.setOperators(Operator.DESCENDANT, Operator.ANCESTOR, Operator.ANCESTOR, Operator.PARENT);
+			break;
+		case 73:
+			q.setLabels("UCP", "PP", "VP");
+			q.setParents(-1, 0, 1);
+			q.setOperators(Operator.DESCENDANT, Operator.ANCESTOR, Operator.ANCESTOR);
+			break;
+		case 74:
+			q.setLabels("UCP", "PP", "NP");
+			q.setParents(-1, 0, 1);
+			q.setOperators(Operator.DESCENDANT, Operator.ANCESTOR, Operator.ANCESTOR);
+			break;
+		case 75:
+			q.setLabels("UCP", "PP", "VP");
+			q.setParents(-1, 0, 0);
+			q.setOperators(Operator.DESCENDANT, Operator.ANCESTOR, Operator.DESCENDANT);
+			break;
+		case 76:
+			q.setLabels("UCP", "PP", "VP");
+			q.setParents(-1, 0, 0);
+			q.setOperators(Operator.DESCENDANT, Operator.ANCESTOR, Operator.CHILD);
+			break;
+		case 77:
+			q.setLabels("NP", "on", "go");
+			q.setParents(-1, 0, 1);
+			q.setOperators(Operator.DESCENDANT, Operator.IMMEDIATE_PRECEDING, Operator.IMMEDIATE_PRECEDING);
+			break;
+		case 78:
+			q.setLabels("right", "RB", "ADVP", "VB", "RB");
+			q.setParents(-1, 0, 1, 2, 1);
+			q.setOperators(Operator.DESCENDANT, Operator.PARENT, Operator.PARENT, Operator.PRECEDING_SIBLING, Operator.IMMEDIATE_FOLLOWING_SIBLING);
+			break;
+		case 79:
+			q.setLabels("NP", "VP");
+			q.setParents(-1, 0);
+			q.setOperators(Operator.DESCENDANT, Operator.IMMEDIATE_PRECEDING_SIBLING);
+			break;
+		case 80:
+			q.setLabels("NP", "VP");
+			q.setParents(-1, 0);
+			q.setOperators(Operator.DESCENDANT, Operator.IMMEDIATE_FOLLOWING_SIBLING);
+			break;
+		case 81:
+			q.setLabels("and", "CC", "NN", "NNS", "NNS", "JJ");
+			q.setParents(-1, 0, 1, 2, 1, 4);
+			q.setOperators(Operator.DESCENDANT, Operator.PARENT, Operator.IMMEDIATE_FOLLOWING_SIBLING, Operator.FOLLOWING_SIBLING, Operator.IMMEDIATE_PRECEDING_SIBLING, Operator.IMMEDIATE_PRECEDING_SIBLING);
+			break;
 		default:
 			break;
 		}
